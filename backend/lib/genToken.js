4a4c9cc6abc22b0cf792e85d186eea2906685407
Abay-Wizard <abaytessema251@gmail.com>
2025-12-13 22:55:47 +0200

    a

--- backend/lib/genToken.js
+++ backend/lib/genToken.js
@@ -3,9 +3,9 @@ const generateToken=async(res)=>{
     const token=jwt.sign({admin:true},process.env.jwt_secret,{expiresIn:'7d'})
     res.cookie('jwt',token,{
         maxAge:60*60*24*7*1000,
-        secure:process.env.NODE_ENV ==='production',
+        secure:true,
         httpOnly:true,
-        samesite:process.env.NODE_ENV ==='production' ? 'none' :'lax'
+        samesite:'none'
     })
 
     return token
