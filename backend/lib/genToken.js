705969b22c465893f82bff13c1ca0cbce82705b0
Abay-Wizard <abaytessema251@gmail.com>
2025-12-14 09:58:34 +0200

    auth updated

--- backend/lib/genToken.js
+++ backend/lib/genToken.js
@@ -1,13 +1,6 @@
 import jwt from 'jsonwebtoken'
 const generateToken=async(res)=>{
     const token=jwt.sign({admin:true},process.env.jwt_secret,{expiresIn:'7d'})
-    res.cookie('jwt',token,{
-        maxAge:60*60*24*7*1000,
-        secure:true,
-        httpOnly:true,
-        samesite:'none'
-    })
-
     return token
 }
 
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.cookies.jwt;
+  const token = localStorage.getItem('token')
   if (!token) {
     return res.status(401).json({ success: false, message: "No token is provided!" });
   }
