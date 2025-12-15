e16f74daa2c86d66206c49842ba35d1f0d7014f3
Abay-Wizard <abaytessema251@gmail.com>
2025-12-15 19:00:18 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import bookRouter from './routes/bookRoute.js'
 dotenv.config()
 const app=express()
 app.use(cors({
-    origin:'http://localhost:5173',  //https://abaytessema.vercel.app
+    origin:'https://abaytessema.vercel.app',  //
     credentials:true  
 }))
 app.use(express.json({limit:'50mb'}))
